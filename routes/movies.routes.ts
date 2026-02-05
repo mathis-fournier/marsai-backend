4a9f804e33d395c406e2e9ae11e0346bc70a5825
evan-sachot <evan.sachot@laplateforme.io>
2026-02-05 10:07:32 +0100

    add multer for upload

--- routes/movies.routes.ts
+++ routes/movies.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import movieController from "../controllers/movies.controller";
-
+import upload from "../config/multer";
 const router = Router();
 
 router.get("/", movieController.getAllMovies);
 router.get("/count", movieController.getMoviesSum);
 router.get("/best", movieController.getBestMovies);
 
-router.post("/", movieController.addMovie);
+router.post("/", upload.single("file"), movieController.addMovie);
 
 export default router;
